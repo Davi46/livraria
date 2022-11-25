af6457eb5362b7ea917946014dfe94b002e3c9f1
Davi46 <davyalmeida.bh@gmail.com>
2022-11-25 19:38:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ import service.LivrariaService;
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        /*carregarProdutos();
-        EstoqueService.listarEstoque();*/
-
-
+    public static void main(String[] args) throws Exceptio { 
+        
         LivrariaService livrariaService = new LivrariaService();
         livrariaService.novaCompra();
         livrariaService.addCliente(new Cliente("Davi Almeida", "MG 12369863", "65366545455", LocalDate.of(1997,3,20)));
